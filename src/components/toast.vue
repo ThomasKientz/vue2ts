fc447d4f0d93d4bb8be32696f70e49dff15c88af
Thomas Kientz <thomas@kientz.fr>
2020-02-09 09:07:33 +0100

    New toast type warning

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -27,7 +27,11 @@ export default {
       } else if (type == 'error') {
         this.color = 'error'
         this.timeout = 4000
+      } else if (type == 'warning') {
+        this.color = 'amber'
+        this.timeout = 3000
       } else {
+        console.log('no toast type :', type)
         return
       }
 
